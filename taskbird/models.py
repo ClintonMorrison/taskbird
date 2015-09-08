d1f908981f261d1aacc28a27e733ac7a12b469e6
clinton <clint.222@hotmail.com>
2015-09-07 23:13:52 -0300

    signup page creates new users and generates demo data

--- taskbird/models.py
+++ taskbird/models.py
@@ -3,33 +3,56 @@ from django.contrib.auth.models import User
 
 
 class Project(models.Model):
-  user = models.ForeignKey(User)
-  title = models.CharField(max_length=50, default="New Project")
-  icon = models.CharField(max_length=50, default="cube")
-  color = models.CharField(max_length=50, default="black")
-  description = models.CharField(max_length=300, blank=True, default="")
-  date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=50, default="New Project")
+    icon = models.CharField(max_length=50, default="cube")
+    color = models.CharField(max_length=50, default="black")
+    description = models.CharField(max_length=300, blank=True, default="")
+    date_created = models.DateTimeField(auto_now_add=True)
 
 
 class Task(models.Model):
-  user = models.ForeignKey(User)
-  title = models.CharField(max_length=50, default="New Task")
-  description = models.CharField(max_length=300, blank=True, default="")
-  type = models.CharField(max_length=50, default="task") #'task' or 'event'
-  done = models.BooleanField(default=False)
-  date_created = models.DateTimeField(auto_now_add=True)
-  date_completed = models.DateTimeField(null=True, blank=True)
-  date_modified = models.DateTimeField(auto_now=True)
-  date_due = models.DateTimeField(null=True, blank=True)
-  priority = models.CharField(max_length=10, default="Normal", choices = (
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=50, default="New Task")
+    description = models.CharField(max_length=300, blank=True, default="")
+    type = models.CharField(max_length=50, default="task") #'task' or 'event'
+    done = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    date_due = models.DateTimeField(null=True, blank=True)
+    priority = models.CharField(max_length=10, default="Normal", choices = (
     ('Low', 'Low'),
     ('Normal', 'Normal'),
     ('High', 'High')
-  ))
-  projects = models.ManyToManyField(Project, blank=True)
+    ))
+    projects = models.ManyToManyField(Project, blank=True)
 
 
 class UserSettings(models.Model):
-  user = models.OneToOneField(User)
-  notes = models.CharField(max_length=1000, blank=True, default="")
-  metadata = models.CharField(max_length=1000, blank=True, default="")
+    user = models.OneToOneField(User)
+    notes = models.CharField(max_length=1000, blank=True, default="")
+    metadata = models.CharField(max_length=1000, blank=True, default="")
+
+
+def generate_demo_data(user):
+    sample_tasks = [
+        ['Start Using Task Bird', 'Thanks for trying Task Bird!'],
+        ['Get The Most Out Of Task Bird', 'See http://taskbird.ca/resources'],
+    ]
+
+    sample_projects = [
+        ['Personal', 'yellow', 'star'],
+        ['School', 'black', 'student'],
+        ['Work', 'black', 'suitcase']
+
+    ]
+
+    for title, color, icon in sample_projects:
+        project = Project(user=user, title=title, color=color, icon=icon)
+        project.save()
+
+    for title, description in sample_tasks:
+        task = Task(user=user, title=title, description=description)
+        task.save()
+
--- taskbird/views.py
+++ taskbird/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as djangoLogin
+from models import User, generate_demo_data
+
+import json
 
 
 def index(request):
@@ -26,12 +29,27 @@ def siteContact(request):
 
 
 def siteSignup(request):
-  return render(request, "site/signup.html", {})
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if email and password:
+            user = User.objects.create_user(username = email, email=email, password=password)
+            user.save()
+            user = authenticate(username=email, password=password)
+            generate_demo_data(user)
+            djangoLogin(request, user)
+            return render(request, "app/index.html", {'user': user})
+        else:
+            return render(request, "site/signup.html", {'error_message': "You must enter a valid name, email, and password."})
+
+    return render(request, "site/signup.html", {})
 
 
 def login(request):
-  username = request.POST.get('username');
-  password = request.POST.get('password');
+  username = request.POST.get('username')
+  password = request.POST.get('password')
   user = authenticate(username=username, password=password)
   if user is not None:
     if user.is_active:
@@ -41,12 +59,6 @@ def login(request):
       return HttpResponseRedirect("/")
   else:
     return render(request, "site/login.html", {})
-
-def signup(request):
-  name = request.POST.get('name')
-  email = request.POST.get('email')
-  password = request.POST.get('password')
-
 
 @login_required
 def appIndex(request):
