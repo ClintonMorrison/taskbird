42e6b80a984ff8c050da15f5dc60808cfd0360d7
clinton <clint.222@hotmail.com>
2015-10-03 02:00:54 -0300

    add content to several pages on website

--- taskbird/forms.py
+++ taskbird/forms.py
@@ -5,3 +5,7 @@ class LoginForm(forms.Form):
 	username = forms.CharField(label='Username', max_length=100)
 	password = forms.CharField(label='Password', max_length=100)
 
+
+class ContactForm(forms.Form):
+	name = forms.CharField(label='Name', max_length=100)
+	message = forms.CharField(label='Name', max_length=100, required=False)
--- taskbird/views.py
+++ taskbird/views.py
@@ -6,71 +6,92 @@ from django.contrib.auth import login as djangoLogin
 from models import User, generate_demo_data
 from django.core.mail import send_mail
 
-import json
+from forms import ContactForm
 
+import json
+import yaml
+import os
+import settings
 
 def index(request):
-  return render(request, "index.html", {})
+    return render(request, "index.html", {})
 
 
 def siteIndex(request):
-  return render(request, "site/index.html", {})
+    return render(request, "site/index.html", {})
 
 
 def siteFeatures(request):
-  return render(request, "site/features.html", {})
+    file_path = os.path.join(settings.STATIC_ROOT, 'content/features.yaml')
+    features = yaml.load(open(file_path, 'r'))
+    #return JsonResponse({'1': features})
+    return render(request, "site/features.html", {'features': features})
 
 
 def siteAbout(request):
-  return render(request, "site/about.html", {})
+    return render(request, "site/about.html", {})
 
 
 def siteContact(request):
-  email = request.POST.get('email')
-  name = request.POST.get('name', '[no name]')
-  message = request.POST.get('message', '[no message]')
+    data = {}
 
-  if email:
-      send_mail('Feedback from "%s"' % name, message, email, ['contact@taskbird.ca'])
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        data['form'] = form
+        if form.is_valid():
+            send_mail(
+                'Feedback from "%s"' % form.cleaned_data.get('name', ''),
+                form.cleaned_data.get('message', ''),
+                'contact@taskbird.ca',
+                ['contact@taskbird.ca']
+            )
 
-  return render(request, "site/contact.html", {})
+    return render(request, "site/contact.html", data)
 
 
 def siteSignup(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
+        first_name = request.POST.get('first_name', '')
+        last_name = request.POST.get('last_name', '')
         email = request.POST.get('email')
         password = request.POST.get('password')
 
         if email and password:
-            user = User.objects.create_user(username = email, email=email, password=password)
+            user = User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                username = email,
+                email = email,
+                password =password
+            )
             user.save()
             user = authenticate(username=email, password=password)
             generate_demo_data(user)
             djangoLogin(request, user)
-            return render(request, "app/index.html", {'user': user})
+            return HttpResponseRedirect("/app/")
         else:
-            return render(request, "site/signup.html", {'error_message': "You must enter a valid name, email, and password."})
+            return render(request, "site/signup.html", {'error_message': "You must enter a valid email, and password."})
 
     return render(request, "site/signup.html", {})
 
 
 def login(request):
-  if request.user.is_authenticated():
-      return HttpResponseRedirect("/app/")
-  username = request.POST.get('username')
-  password = request.POST.get('password')
-  user = authenticate(username=username, password=password)
-  if user is not None:
-    if user.is_active:
-      djangoLogin(request, user)
-      return HttpResponseRedirect("/app/")
+    if request.user.is_authenticated():
+        return HttpResponseRedirect("/app/")
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            djangoLogin(request, user)
+            return HttpResponseRedirect("/app/")
+        else:
+            return HttpResponseRedirect("/")
     else:
-      return HttpResponseRedirect("/")
-  else:
-    return render(request, "site/login.html", {})
+        return render(request, "site/login.html", {})
 
 @login_required
 def appIndex(request):
-  return render(request, "app/index.html", {'user': request.user})
+    return render(request, "app/index.html", {'user': request.user})
 
