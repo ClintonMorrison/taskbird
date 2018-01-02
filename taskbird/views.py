9d629c1c6421a8ab39a1be1db4f0ab4459393de9
Clinton Morrison <contact@clintonmorrison.com>
2018-01-02 04:26:53 +0000

    remove unneeded login code

--- taskbird/views.py
+++ taskbird/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as djangoLogin
-import taskbird.email
-
-from .forms import ContactForm
 
 import yaml
 import os
@@ -14,46 +11,25 @@ from . import settings
 def index(request):
     return render(request, "index.html", {})
 
-
 def siteIndex(request):
     return render(request, "site/index.html", {})
-
 
 def siteFeatures(request):
     file_path = os.path.join(settings.BASE_STATIC_PATH, 'content/features.yaml')
     features = yaml.load(open(file_path, 'r'))
     return render(request, "site/features.html", {'features': features})
 
-
 def siteAbout(request):
     return render(request, "site/about.html", {})
-
 
 def siteContact(request):
     return render(request, "site/contact.html", {})
 
 def login(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect("/app/")
+    if request.user.is_authenticated():
+        return HttpResponseRedirect("/app")
 
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            djangoLogin(request, user)
-            return HttpResponseRedirect("/app/")
-        else:
-            return HttpResponseRedirect("/")
-    else:
-        return render(request, "site/login.html", {})
-
-def request_password_reset(request):
-    data = {}
-    return render(request, "site/requestPasswordReset.html", data)
-
-def password_reset(request):
-    return JsonResponse({'a': 'a'})
+    return render(request, "site/login.html", {})
 
 @login_required
 def appIndex(request):
