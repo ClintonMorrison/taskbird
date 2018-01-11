07c2460a9a7f5396b434d905fd62cad0a3a29efd
Clinton Morrison <contact@clintonmorrison.com>
2018-01-11 01:32:40 +0000

    fix reporting script to work with python3

--- scripts/reportUserData.py
+++ scripts/reportUserData.py
@@ -22,24 +22,22 @@ if len(sys.argv) == 2:
   path, userID = sys.argv
   userID = int(userID)
 
-print '=== USERS ==='
+print('=== USERS ===')
 
 for user in User.objects.all():
   header = '     '
   if userID == user.id: header = ' >>> ' 
-  print '%s%d: %s %s <%s>' % (header, user.id, user.first_name, user.last_name, user.email)
+  print('%s%d: %s %s <%s>' % (header, user.id, user.first_name, user.last_name, user.email))
 
 if userID:
-  print '\n=== TASKS ==='
+  print('\n=== TASKS ===')
   for task in models.Task.objects.filter(user__id=userID):
     date = ''
     if task.date_due: date = '[@ %s]' % task.date_due
-    print '>', task.title, date
+    print('>', task.title, date)
     try:
-      print task.description
-      if task.description: print ''
+      print(task.description)
+      if task.description: print('')
     except:
       pass
-
-#print models.Task.objects.all()
 
