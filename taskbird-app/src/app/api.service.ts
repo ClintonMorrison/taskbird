79d24a534e89b84d389213874361833ef2168903
Clinton Morrison <contact@clintonmorrison.com>
2018-06-21 09:11:30 -0400

    redirect to login on 401 failure

--- taskbird-app/src/app/api.service.ts
+++ taskbird-app/src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch'
 import { ApiResponse } from './models/api-response';
 
 @Injectable()
@@ -13,29 +14,44 @@ export class ApiService {
   ) {}
 
   get(resource): Observable<any> {
-    return this.http.get(
+    return this.request(
+      'get',
       `${this.base}/${resource}/?format=json`
     );
   }
 
   put(resource, id, data): Observable<any> {
-    return this.http.put(
+    return this.request(
+      'put',
       `${this.base}/${resource}/${id}?format=json`,
       data
     );
   }
 
   post(resource, data): Observable<any> {
-    return this.http.post(
+    return this.request(
+      'post',
       `${this.base}/${resource}/?format=json`,
       data
     );
   }
 
   delete(resource, id): Observable<any> {
-    return this.http.delete(
+    return this.request(
+      'delete',
       `${this.base}/${resource}/${id}?format=json`
     );
+  }
+
+  request(method: string, url: string, data = {}): Observable<any> {
+    return this.http[method](
+      url,
+      data
+    ).catch(error => {
+      if (error.status === 401) {
+        window.location.pathname = '/login/';
+      }
+    });
   }
 
   path(resource) {
