31d4d535694782131dc53f2194eb175c84d1455c
Clinton Morrison <contact@clintonmorrison.com>
2018-06-23 13:58:08 -0400

    fix build script, and rebuild

--- taskbird-app/build_for_prod.sh
+++ taskbird-app/build_for_prod.sh
@@ -1,3 +1,3 @@
 rm -rf dist
-ng build --prod --build-optimizer --base-href=/v2/ && rm -rf ../dist && mv dist ../v2
+ng build --prod --build-optimizer --base-href=/v2/ && rm -rf ../v2 && mv dist ../v2
 
