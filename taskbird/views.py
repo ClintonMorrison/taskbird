1f8eaf02e747d0f677a5ec42d71fa34b8a9d6607
Clinton Morrison <contact@clintonmorrison.com>
2018-12-12 21:40:40 -0500

    make sure app redirects to v2

--- taskbird/views.py
+++ taskbird/views.py
@@ -6,22 +6,28 @@ import yaml
 import os
 from . import settings
 
+
 def index(request):
     return render(request, "index.html", {})
 
+
 def siteIndex(request):
     return render(request, "site/index.html", {})
+
 
 def siteFeatures(request):
     file_path = os.path.join(settings.BASE_STATIC_PATH, 'content/features.yaml')
     features = yaml.load(open(file_path, 'r'))
     return render(request, "site/features.html", {'features': features})
 
+
 def siteAbout(request):
     return render(request, "site/about.html", {})
 
+
 def siteContact(request):
     return render(request, "site/contact.html", {})
+
 
 def login(request):
     if request.user.is_authenticated():
@@ -29,7 +35,8 @@ def login(request):
 
     return render(request, "site/login.html", {})
 
+
 @login_required
 def appIndex(request):
-    return render(request, "app/index.html", {'user': request.user})
+    return HttpResponseRedirect("/v2/")
 
