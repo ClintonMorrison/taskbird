e25c34761976db1e86673469679e5ad327f39239
Clinton Morrison <clintonmorrison2@gmail.com>
2025-05-03 13:08:08 -0400

    update taskbird build script

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@ source version.sh
 
 echo "Building version: $VERSION"
 
-docker stop taskbird
-docker rm taskbird
+#docker stop taskbird
+# docker rm taskbird
 
-docker build -t "clintonmorrison/projects:taskbird-$VERSION" . &&
-  docker push "clintonmorrison/projects:taskbird-$VERSION" &&
-  echo "Running container..." &&
-  sh run_container.sh
+TAG="$DOCKER_REGISTRY_PROJECTS/taskbird:$VERSION"
+
+docker build --platform linux/amd64 -t "$TAG" . &&
+  docker push "$TAG"
+
+# echo "Running container..." &&
+#   sh run_container.sh
